68c180abf885ecf2cb9cae06a3e85febb9e94698
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-05-27 08:09:35 -0300

    Wine
    Correção de erros de compilação em Vulkan e ntdll syscalls
    - Corrigido incompatibilidade de tipos em dlls/win32u/vulkan.c
    - Atualizado dlls/ntdll/ntsyscalls.h via make_specfiles para resolver referências indefinidas
    - Corrigido declaração após comando em dlls/ntdll/unix/server.c
    - Removido variáveis não utilizadas em dlls/ntdll/unix/esync.c

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=dbffe47ce4c6ae60c92be4ac6fca6c01062b9b08
+GIT_COMMIT=1d64dc7b614dbe6674c37352152ab154250cd00a
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
