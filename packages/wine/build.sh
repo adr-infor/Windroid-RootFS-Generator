944f1ab30a99e89532314d3435b0bc6c24b06eef
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-05-28 02:00:51 -0300

    Wine
    wow64: sync and virtual memory updates

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=68dc8b6eb603e45ebd16a2645d85c66266c9defb
+GIT_COMMIT=051e45abb1c51de107e519e7bebba6178ace2f0b
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
